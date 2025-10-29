f4c2a9367860560cae06e4806601ee62081d00ee
hdemarcay <hdemarcay@ufl.edu>
2025-10-29 11:42:01 -0400

    Added src file + main changes.

--- DSA_Project2_Group60/src/main.cpp
+++ DSA_Project2_Group60/src/main.cpp
@@ -99,7 +99,7 @@ int main() {
     // Loads the heap and measures the time before and after loading the heap.
     maxHeap heap;
     Clock::time_point beforeLoad = Clock::now();
-    heap.loadHeap("Electric_Vehicle_Population_Data_attempting.csv");
+    heap.loadHeap("../Electric_Vehicle_Population_Data_attempting.csv");
     Clock::time_point afterLoad = Clock::now();
     Duration loadTime = chrono::duration_cast<Duration>(afterLoad - beforeLoad);
     cout << "It took "<< setprecision(3) << loadTime.count() << " seconds to load the data." << endl;
@@ -409,7 +409,7 @@ int main() {
 
             if (validParameter == true) {
                 // Final comparison
-                cout<<depthTimeString<<"search DSF!" <<endl;
+                cout<<"\n"<<depthTimeString<<"search DSF!" <<endl;
                 cout<<breadthTimeString<<"search BSF!"<<endl;
 
                 if (breadthTime > depthTime) {
